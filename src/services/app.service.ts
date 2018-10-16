11177ca3e9ce74cddaff994c1afad22ac12b6d1f
unknown <jeancarlorjs@gmail.com>
2018-10-16 16:12:25 -0500

    hey

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppService {
-    // gateway = "https://nodejs.fitcoapp.net";
-    gateway = "http://172.16.17.243:9000";
-    setEstablishmentId = 4;
-    // setEstablishmentId = 265;
+    gateway = "https://nodejs.fitcoapp.net";
+    // gateway = "http://172.16.17.243:9000";
+    // setEstablishmentId = 4;
+    setEstablishmentId = 84;
 }
